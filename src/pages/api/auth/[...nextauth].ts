a3773d9c88365d8b90180aeea307f509c848c362
samuel vermeuln <70477826+samuelvermeuln@users.noreply.github.com>
2021-12-28 23:20:37 -0300

    config fauna

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
+import { query as q } from 'faunadb'
+import { fauna } from '../../../services/fauna';
+
 export default NextAuth({
   providers: [
     GithubProvider({
@@ -13,4 +16,20 @@ export default NextAuth({
       },
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+
+      console.log("EMAIL =>> ", email);
+      
+      // FQL cheat sheet => e a forma de inserir no banco
+      await fauna.query(
+        q.Create(
+          q.Collection('user'),
+          {data: {email}}
+        )
+      )
+
+      return true
+    },
+  }
 });
--- src/services/fauna.ts
+++ src/services/fauna.ts
@@ -1,9 +1,9 @@
 import { Client } from "faunadb";
 
-export const faunadb = new Client({
-  secret: process.env.FaunaDB_KEY,
-//   domain: "db.fauna.com",
-//   // NOTE: Use the correct domain for your database's Region Group.
-//   port: 443,
-//   scheme: "https",
+export const fauna = new Client({
+  secret: process.env.FAUNADB_KEY,
+  // domain: "db.eu.fauna.com",
+  // // NOTE: Use the correct domain for your database's Region Group.
+  // port: 443,
+  // scheme: "https",
 });
